f93f5549975688a3ee0a914a94c714b70eb8cd12
Marat Gaffarov <gaffarov142004@gmail.com>
2023-12-07 18:00:39 +0300

    empty commit

--- Bang_Avalonia/obj/Debug/net7.0/Bang_Avalonia.AssemblyInfo.cs
+++ Bang_Avalonia/obj/Debug/net7.0/Bang_Avalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bang_Avalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09cec145906f083cdd3d17da7e9694be186da15c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bang_Avalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bang_Avalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
